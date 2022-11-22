1fe09fa7ed06967702a0a83c0677b3d311a1d066
sofiaoreis <sofiareis1994@gmail.com>
2022-11-22 22:51:56 +0100

    setup package

--- secomlint/setup.py
+++ secomlint/setup.py
@@ -1,14 +1,25 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='secomlint',
-    version='0.1.0',    
+    version='0.1.2',    
     description='A linter for security commit messages',
     url='https://github.com/tqrg/secomlint',
     author='Sofia Reis',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author_email='sofiareis1994@gmail.com',
+    license='MIT',
     packages=['secomlint'],
-    install_requires=['spacy'],
+    include_package_data=True,
+    package_data={'secomlint': ['entities/patterns.jsonl', 'config/rules.yml']},
+    install_requires=[
+        'spacy',
+        'pyyaml'
+],
     entry_points={
         'console_scripts': [
             'secomlint = secomlint.__main__:main'
